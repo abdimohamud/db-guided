b16a588a85a124317505a1a057bb9d95722504ca
unknown <abdinajibmohamud@gmail.com>
2020-10-06 10:22:02 -0700

    created migrations

---
+++ migrations/20201006100413_init.js
@@ -0,0 +1,12 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("menu_items", (tbl) => {
+    tlb.increcements(); //id
+    tbl.string("name", 512).notNullable().unique().index(); //name
+    tbl.decimal("price", 8, 2);
+    tbl.boolean("available").defaultTo(false);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("menu_items");
+};
