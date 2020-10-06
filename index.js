97fce2adcb4c60157e451993bb9dea8c81e9df00
unknown <abdinajibmohamud@gmail.com>
2020-10-06 09:27:16 -0700

    server up and running

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "RUNNING" });
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`API RUNNING ON PORT ${PORT}`));
